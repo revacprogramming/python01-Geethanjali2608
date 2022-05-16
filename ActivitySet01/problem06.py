46dc279bab8696af5ae49fc9ae7b8979c6b9c9f6
Geethanjali2608 <geethanjali2608@gmail.com>
2022-05-16 09:44:11 +0530

    p6

--- ActivitySet01/problem06.py
+++ ActivitySet01/problem06.py
@@ -1,5 +1,19 @@
 # Loops & Iterators
 
+x=float(input("Enter score:"))
+if x>=0.9:
+    print("A")
+elif x>=0.8 and x<0.9:
+    print("B")
+elif x>=0.7 and x<0.8:
+    print("C")    
+elif x>=0.6 and x<0.7:
+    print("D")    
+elif x<0.6:
+    print("F")
+else:
+    print("invaid input")
+    
 '''while True:
     print("loop")
     line=input('>')
@@ -18,9 +32,9 @@ print("program terminated")'''
     print(line)
 print('Done!')'''
 
-friends = ['Joseph', 'Glenn', 'Sally']
+'''friends = ['Joseph', 'Glenn', 'Sally']
 for a in friends:
     print('Happy New Year:', a)
-print('Done!')
+print('Done!')'''
 
 
