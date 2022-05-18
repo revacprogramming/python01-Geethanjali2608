3ae10e2799fac07ad4a5f4b6b076a34a4aab056f
Geethanjali2608 <96220572+Geethanjali2608@users.noreply.github.com>
2022-05-18 10:31:51 +0000

    user defined funct

--- ActivitySet01/problem05.py
+++ ActivitySet01/problem05.py
@@ -1,12 +1,11 @@
-# Functions
+def computepay(hrs,rate):
+    if hrs>40:
+        pay=40*rate+(hrs-40)*rate*1.5
+    else:
+        pay=hrs*rate
+    return pay
+hrs=float(input("enter hours:"))
+rate=float(input("enter rate:"))
+pay=computepay(hrs,rate)
+print(pay)
 
-
-def computepay(h, r):
-    pass  # ...
-
-
-hrs = float(input("Enter hours? "))
-rte = float(input("Enter rate per hour? "))
-
-p = computepay(hrs, rte)
-print("Pay", p)
