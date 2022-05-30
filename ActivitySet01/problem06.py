5265348a61a9a53019bfbd2de7f86949d382023f
Geethanjali2608 <96220572+Geethanjali2608@users.noreply.github.com>
2022-05-30 03:19:10 +0000

    program6

--- ActivitySet01/problem06.py
+++ ActivitySet01/problem06.py
@@ -1,47 +1,33 @@
 # Loops & Iterators
 
-'''x=float(input("Enter score:"))
-if x>=0.9:
-    print("A")
-elif x>=0.8 and x<0.9:
-    print("B")
-elif x>=0.7 and x<0.8:
-    print("C")    
-elif x>=0.6 and x<0.7:
-    print("D")    
-elif x<0.6:
-    print("F")
-else:
-    print("invaid input")'''
-
-
-'''while True:
-    print("loop")
-    line=input('>')
-    if line=='done':
+largest = None
+smallest = None 
+while True:
+    num = input("Enter a number: ")
+    if num== "done":
         break
-    print(line)
-print("program terminated")'''
-
-'''while True:
-    line = input('> ')
-    print("loop")
-    if line[0] == '#':
+    try:
+        num=int(num)  
+  
+    except:
+        print("Invalid input")
         continue
-    if line == 'done':
-        break
-    print(line)
-print('Done!')'''
+    if largest==None or num>largest:
+        largest=num
+    if smallest==None or num<smallest:
+        smallest=num
+print("Maximum is",largest)
+print("Minimum is",smallest)
 
 
 
-def computepay(hrs,rate):
-    if hrs>40:
-        pay=40*rate+(hrs-40)*rate*1.5
-    else:
-        pay=hrs*rate
-    return pay
-hrs=float(input("enter hours:"))
-rate=float(input("enter rate:"))
-pay=computepay(hrs,rate)
-print(pay)
+
+
+
+
+
+
+    
+  
+  
+  
--- ActivitySet01/problem13.py
+++ ActivitySet01/problem13.py
@@ -1,2 +1,4 @@
 # Network Programming
 # https://www.py4e.com/lessons/network
+for i in range(5):
+   print(i,end="")
